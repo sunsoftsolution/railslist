19d9fb215072423d08b84bd802d8f8bee62a6e17
SunsoftSolution <info@sunsoftsolution.com>
2009-12-19 00:27:27 +0530

    change

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+
 ActiveRecord::Schema.define(:version => 0) do
   create_table "categories", :force => true do |t|
     t.string "name"
